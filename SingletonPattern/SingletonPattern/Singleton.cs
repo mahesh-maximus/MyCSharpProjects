959126a8e8f5a976f99d69d3a280a855239125b1
maheshdharmasena <mahesh.ksl@gmail.com>
2014-01-07 17:02:05 +0530

    Added

---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+
+        private Singleton instance;
+
+        private Singleton() { }
+
+        public static synchronized Singleton getInstance() {
+        }
+    }
+}
